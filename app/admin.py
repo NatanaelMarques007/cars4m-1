b13e6eb51034bc3f003872308a4dd01676b47f1d
Capitão <marquesnatanael373@gmail.com>
2024-11-14 10:04:52 -0300

    Atualizações

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from app.models import Car
 
 # Register your models here
+class CarFilter(admin.ModelAdmin):
+    list_display = ("id", "model", "brand", "year")
+    list_display_links =("id", "model", "brand", "year")
+    list_filter = ("id", "model", "brand", "year")
+    search_fields = ("model", "brand")
 
-admin.site.register(Car)
+
+admin.site.register(Car, CarFilter)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Car
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    cars = Car.objects.all()
+    return render(request, 'index.html', {'cars': cars})
+
+def devs(request):
+    if request.user.is_authenticated:
+        return render(request, 'desenvolvedores.html')
+    else:
+        return redirect('/admin/login/?next=/')
