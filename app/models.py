94f721cf314c01dc21db5c1e74e17bf569c4f5ac
Capitão <marquesnatanael373@gmail.com>
2024-11-21 07:48:44 -0300

    New Atualizations

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
 class Car(models.Model):
     model = models.CharField(max_length=50, null=False)
-    brand= models.CharField(max_length=50, null=False)
+    brand = models.CharField(max_length=50, null=False)
     year = models.IntegerField(null=False)
     filename = models.CharField(max_length=50, null=False)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.model + 'ano: ' + str(self.year)
-   
+        return f"{self.model} - Ano: {self.year}"
